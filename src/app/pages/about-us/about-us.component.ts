3f865a613e378e0c1c06d651b5099c454d40effe
bakasa <a.abduraghman@itqan.dev>
2025-09-22 16:55:34 +0200

    QAD-80: SEO and site social/search engine preview

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [CommonModule, TranslateModule ],
+  imports: [CommonModule, TranslateModule, NzIconModule],
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss']
 })
-export class AboutUsComponent {
-  constructor() { }
+export class AboutUsComponent implements OnInit {
+  currentLang: 'ar' | 'en' = 'ar';
+
+  constructor(private translateService: TranslateService) { }
+
+  ngOnInit() {
+    this.currentLang = this.translateService.currentLang as 'ar' | 'en';
+    
+    // Subscribe to language changes
+    this.translateService.onLangChange.subscribe((event) => {
+      this.currentLang = event.lang as 'ar' | 'en';
+    });
+  }
 } 
