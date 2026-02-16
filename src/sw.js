8569973eeb4cee955a199b701a75e9bac7463852
abubakr-itqan <a.abduraghman@itqan.dev>
2026-02-16 15:45:04 +0200

    Self-Destructing SW Fix - Replace deleted sw.js with self-destructing version to decommission old cached service workers

---
+++ src/sw.js
@@ -0,0 +1,15 @@
+// Self-destructing SW - replaces old caching SW, clears everything, removes itself
+self.addEventListener('install', function() { self.skipWaiting(); });
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys()
+      .then(function(names) {
+        return Promise.all(names.map(function(n) { return caches.delete(n); }));
+      })
+      .then(function() { return self.registration.unregister(); })
+      .then(function() { return self.clients.matchAll(); })
+      .then(function(clients) {
+        clients.forEach(function(c) { c.navigate(c.url); });
+      })
+  );
+});
